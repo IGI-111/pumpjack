dc4f652c26ed397d8188d0118d65ff26c20ddefa
IGI-111 <igi-111@protonmail.com>
2024-11-01 14:45:19 +0100

    Enable walletconnect

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -14,8 +14,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Provider, CHAIN_IDS } from "fuels";
 import { FUEL_PROVIDER_URL } from "./constants.ts";
 import { createConfig, http, injected } from "@wagmi/core";
-// import { mainnet } from "@wagmi/core/chains";
-// import { walletConnect } from "@wagmi/connectors";
+import { mainnet } from "@wagmi/core/chains";
+import { walletConnect } from "@wagmi/connectors";
 import type { Config as WagmiConfig } from "@wagmi/core";
 import "./index.css";
 import App from "./App.tsx";
@@ -28,27 +28,26 @@ const networks = [
   },
 ];
 const FUEL_CONFIG = createFuelConfig(() => {
-  // FIXME: from farm
-  // const WalletConnectProjectId = "35b967d8f1770c100a00300bee56e579";
-  // const wagmiConfig = createConfig({
-  //   syncConnectedChain: false,
-  //   chains: [mainnet],
-  //   transports: {
-  //     [mainnet.id]: http(),
-  //   },
-  //   connectors: [
-  //     injected({ shimDisconnect: false }),
-  //     // walletConnect({
-  //     //   projectId: WalletConnectProjectId,
-  //     //   metadata: {
-  //     //     name: "Pumpjack",
-  //     //     description: "Blackjack on the Fuel network.",
-  //     //     url: "https://pumpjack.ly/",
-  //     //     icons: ["https://connectors.fuel.network/logo_white.png"],
-  //     //   },
-  //     // }),
-  //   ],
-  // });
+  const WalletConnectProjectId = "d5a9f3615ed79d73e5b2737c36584659";
+  const wagmiConfig = createConfig({
+    syncConnectedChain: false,
+    chains: [mainnet],
+    transports: {
+      [mainnet.id]: http(),
+    },
+    connectors: [
+      injected({ shimDisconnect: false }),
+      walletConnect({
+        projectId: WalletConnectProjectId,
+        metadata: {
+          name: "Pumpjack",
+          description: "Blackjack on the Fuel network.",
+          url: "https://pumpjack.link/",
+          icons: ["https://connectors.fuel.network/logo_white.png"],
+        },
+      }),
+    ],
+  });
 
   const fuelProvider = Provider.create(FUEL_PROVIDER_URL);
 
@@ -60,25 +59,25 @@ const FUEL_CONFIG = createFuelConfig(() => {
   const fueletWalletConnector = new FueletWalletConnector();
   const fuelWalletConnector = new FuelWalletConnector();
   const bakoSafeConnector = new BakoSafeConnector();
-  // const burnerWalletConnector = new BurnerWalletConnector({ fuelProvider });
-  // const walletConnectConnector = new WalletConnectConnector({
-  //   projectId: WalletConnectProjectId,
-  //   wagmiConfig: wagmiConfig as WagmiConfig,
-  //   ...externalConnectorConfig,
-  // });
-  // const solanaConnector = new SolanaConnector({
-  //   projectId: WalletConnectProjectId,
-  //   ...externalConnectorConfig,
-  // });
+  const burnerWalletConnector = new BurnerWalletConnector({ fuelProvider });
+  const walletConnectConnector = new WalletConnectConnector({
+    projectId: WalletConnectProjectId,
+    wagmiConfig: wagmiConfig as WagmiConfig,
+    ...externalConnectorConfig,
+  });
+  const solanaConnector = new SolanaConnector({
+    projectId: WalletConnectProjectId,
+    ...externalConnectorConfig,
+  });
   const userAgent = navigator.userAgent.toLowerCase();
   const isMobile = /(iphone|android|windows phone)/.test(userAgent);
 
   return {
     connectors: [
       fueletWalletConnector,
-      // walletConnectConnector,
-      // solanaConnector,
-      // burnerWalletConnector,
+      walletConnectConnector,
+      solanaConnector,
+      burnerWalletConnector,
       ...(isMobile ? [] : [fuelWalletConnector, bakoSafeConnector]),
     ],
   };
